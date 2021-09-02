6729bee48376dd587c4adb9f90f5cbb5034d214e
nssiyam <nurussalamat@iut-dhaka.edu>
2021-09-03 01:44:23 +0600

    third commit

--- js/book.js
+++ js/book.js
@@ -1,3 +1,8 @@
+const noResultMsg = document.getElementById('no-result-msg');
+noResultMsg.style.display = 'none';
+const resultMsg = document.getElementById('result-msg');
+resultMsg.style.display = 'none';
+
 const searchBook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
@@ -16,19 +21,24 @@ const displaySearchResult = data => {
     searchResult.textContent = '';
     const books = data.docs;
 
+    resultMsg.style.display = 'block';
+    resultMsg.innerHTML = `
+        <p>Total Results: ${data.numFound} | Showing Results from 1 to ${data.numFound}</p>
+    `;
+    
     books.forEach(book => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card h-100">
-            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
-            <div class="card-body text-center">
-                <h5 class="card-title fs-3">${book.title}</h5>
-                <p class="card-text"><span class="fw-bold">Author: </span>${book.author_name} </br>
-                <span class="fw-bold">Publisher: </span>${book.publisher} </br>
-                <span class="fw-bold">First Published: </span>${book.first_publish_year}</p>
+            <div class="card h-100">
+                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5 class="card-title fs-3">${book.title}</h5>
+                    <p class="card-text"><span class="fw-bold">Author: </span>${book.author_name} </br>
+                    <span class="fw-bold">Publisher: </span>${book.publisher} </br>
+                    <span class="fw-bold">First Published: </span>${book.first_publish_year}</p>
+                </div>
             </div>
-        </div>
         `;
         searchResult.appendChild(div);
     })
