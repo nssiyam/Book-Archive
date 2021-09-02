0755537dca207527988f507b83015648cdf17442
nssiyam <nurussalamat@iut-dhaka.edu>
2021-09-03 01:22:15 +0600

    third commit

--- js/book.js
+++ js/book.js
@@ -8,5 +8,28 @@ const searchBook = () => {
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displaySearchResult(data))
+}
+
+const displaySearchResult = data => {
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+    const books = data.docs;
+
+    books.forEach(book => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+            <div class="card-body text-center">
+                <h5 class="card-title fs-3">${book.title}</h5>
+                <p class="card-text"><span class="fw-bold">Author: </span>${book.author_name} </br>
+                <span class="fw-bold">Publisher: </span>${book.publisher} </br>
+                <span class="fw-bold">First Published: </span>${book.first_publish_year}</p>
+            </div>
+        </div>
+        `;
+        searchResult.appendChild(div);
+    })
 }
